1de6e706e1b734670df44242a009bfe09e960a27
Fábio Santos <fabio-s-santos@telecom.pt>
2014-02-17 23:59:25 +0000

    can run iifes, eval expressions

--- test/sjedon_test.js
+++ test/sjedon_test.js
@@ -3,10 +3,11 @@
 
 var ok = require('assert')
 var sinon = require('sinon')
+var esprima = require('esprima')
 var Sjedon = require('../lib/sjedon.js')
 
 describe('Sjedon class', function () {
-    it('Can be called or new\'d. Result is always Sjedon instance', function() {
+    it('Can be called or new\'d. Result is always a Sjedon instance', function() {
         /* jshint newcap:false */
         // tests here
         var called = Sjedon()
@@ -16,10 +17,31 @@ describe('Sjedon class', function () {
     })
 })
 
-describe('Sjedon instances', function () {
-    var sjedon
-    beforeEach(function () { sjedon = new Sjedon() })
-
-    sinon.spy(sjedon)
+describe('code:', function () {
+    function aSjedon(ast) {
+        if (typeof ast === 'string') {
+            ast = esprima.parse(ast);
+        }
+        return new Sjedon(ast);
+    }
+    describe('evalExpression', function () {
+        it('literals', function () {
+            var sjedon = aSjedon('3');
+            ok.equal(sjedon.evalExpression(sjedon.ast.body[0].expression), 3);
+            sjedon = aSjedon('"3"')
+            ok.equal(sjedon.evalExpression(sjedon.ast.body[0].expression), "3");
+            sjedon = aSjedon("'3'")
+            ok.equal(sjedon.evalExpression(sjedon.ast.body[0].expression), "3");
+        });
+        // TODO
+    });
+    it('functions and returns', function () {
+        var sjedon = aSjedon('(function () { return 3; })');
+        var result = sjedon.callFunction(sjedon.ast.body[0].expression);
+        ok.equal(result, 3);
+        ok.equal(typeof result, 'number');
+    });
 })
+
+
 
